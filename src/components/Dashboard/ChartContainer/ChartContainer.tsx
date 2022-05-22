0419b60867d959925e674926fb19c03159d60db7
Swarnava Ghosh <swarnava98@gmail.com>
2022-05-22 23:15:08 +0530

    fix(build): fixes build issue for eslint

--- src/components/Dashboard/ChartContainer/ChartContainer.tsx
+++ src/components/Dashboard/ChartContainer/ChartContainer.tsx
@@ -9,7 +9,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 import PriceChart from "./PriceChart/PriceChart";
-import { subDays } from "date-fns";
+
 import {
     getHistoricalDataSymbols,
     getHistoricalDataDateLimits,
